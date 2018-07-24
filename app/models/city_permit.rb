08229582985ba1fd3ccf79661bfbffbabcf7c7bc
Bryce Kerley <bkerley@brycekerley.net>
2018-07-23 21:04:53 -0400

    import and fixup city permits

--- app/models/city_permit.rb
+++ app/models/city_permit.rb
@@ -1,4 +1,5 @@
 class CityPermit < ApplicationRecord
+  has_one :permit
 
   def self.initial_import!
     permits = SODA_CLIENT.get('x37d-q5fz',
@@ -12,11 +13,11 @@ class CityPermit < ApplicationRecord
   end
 
   def self.update_import!
-    newest_permit = CityPermit.order(reviewstatuschangeddate: :desc).one
+    newest_permit = CityPermit.order(reviewstatuschangeddate: :desc).first
     since_date = newest_permit.reviewstatuschangeddate.rfc3339
 
     permits = SODA_CLIENT.get('x37d-q5fz', {
-                                '$order' => 'reviewstatuschangeddate DESC',
+                                '$order' => 'reviewstatuschangeddate ASC',
                                 '$where' =>
                                   "reviewstatuschangeddate > '#{since_date}'"
                               })
@@ -25,6 +26,7 @@ class CityPermit < ApplicationRecord
       cs = self.new
       cs.attributes = permit.to_hash
       cs.save!
+      cs.fixup!
     end
   end
 
@@ -32,24 +34,37 @@ class CityPermit < ApplicationRecord
     Permit.delete_all
     File.open(Rails.root.join('log', 'fixup_permits.log'), 'w') do |l|
       self.in_batches.each_record do |cp|
-        p = Permit.new
-        begin
-          addr = MiamiDadeGeo::Address.new_from_address cp.propertyaddress
-          p.location = "POINT(#{addr.long} #{addr.lat})"
-        rescue => e
-          l.puts e
-          l.puts cp.propertyaddress
-        end
-        p.status_date = cp.reviewstatuschangeddate
-        p.city_permit = cp
-        p.address = cp.propertyaddress
-        p.save!
-        if p.location
-          print '.'
-        else
-          print 'x'
-        end
+        cp.fixup
       end
+    end
+  end
+
+  # single permit fixup
+  def fixup!
+    if self.permit
+      print '-'
+      return
+    end
+
+    p = self.build_permit
+
+    begin
+      addr = MiamiDadeGeo::Address.new_from_address self.propertyaddress
+      p.location = "POINT(#{addr.long} #{addr.lat})"
+    rescue => e
+      Rails.logger.info e
+      Rails.logger.info self.propertyaddress
+    end
+
+    p.status_date = self.reviewstatuschangeddate
+    p.address = self.propertyaddress
+
+    p.save!
+
+    if p.location
+      print '.'
+    else
+      print 'x'
     end
   end
 end
